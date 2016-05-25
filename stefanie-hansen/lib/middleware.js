46a319ddb0977c84bece58ad25fd26ab98ba9cac
Stefanie Hansen <stefuhnee@gmail.com>
2016-05-24 20:29:13 -0700

    all tests working, broke post and put routes into a router so all other routes would receive a 404 response

---
+++ stefanie-hansen/lib/middleware.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// for testing
+let requestObj = exports.requestObj = {};
+
+exports.parse = function jsonParser(req, res, next) {
+  let dataBody = '';
+  req.on('data', (data) => {
+    dataBody += data.toString();
+  });
+  req.on('end', () => {
+    try {
+      req.body = requestObj.body = JSON.parse(dataBody);
+      req.status = requestObj.status = 200;
+      if (next) next();
+    } catch(err) {
+      req.body = requestObj.err = {"Message":"invalid json"};
+      req.status = requestObj.statusErr = 400;
+      if (next) next();
+    }
+  });
+};
